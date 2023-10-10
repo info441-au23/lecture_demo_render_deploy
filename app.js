32c4e24b9652d926f3a2d1dc5192bbb00bba3ec7
Tejus Parikh <tejus@vijedi.net>
2023-10-10 09:38:37 -0700

    Adding a new endpoint for Hello
    New endpoint

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.static('public'));
 
+app.get('/hello', (req, res) => {
+    const name = req.query.name;
+    res.type('html');
+    res.send(`
+        <html>
+            <head>
+                <title>Hello ${name}</title>
+            </head>
+            <body>
+                Hello ${name}.
+            </body>
+        </html>
+    `);
+
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
